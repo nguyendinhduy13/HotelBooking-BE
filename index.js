bd2ee284295638c6346e766c3568ea75080845ba
DuyChamHocAC <duy.tran@apps-cyclone.com>
2023-11-19 16:06:25 +0700

    create hotel

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.use((req, res, next) => {
 async function StartApp() {
   const router = require("./src/routers/index");
   router(app);
-  
+
+  //get ipv4 and update at be and fe
   // const api = require("./src/config/api");
   // const ipv4 = await api.UpdateAPI_URL();
 
