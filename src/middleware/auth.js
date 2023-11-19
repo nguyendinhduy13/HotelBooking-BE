02ae8b4fd2d5fef7c771d6f9842e8ace7e39caa7
nguyendinhduy13 <20521236@gm.uit.edu.vn>
2023-11-19 16:14:52 +0700

    add verify token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,9 +10,28 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, config.TOKEN_KEY);
     req.auth = decoded;
-
   } catch (err) {
-    if(err.message == "jwt expired") {
+    if (err.message == "jwt expired") {
+      return res.status(401).send("Token expired");
+    }
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
+};
+
+const verifyTokenAdmin = (req, res, next) => {
+  const token = req.body.token || req.query.token || req.headers["x-token"];
+  if (!token) {
+    return res.status(403).send("A token is required for authentication");
+  }
+  try {
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    req.auth = decoded;
+    if (decoded.role != "admin") {
+      return res.status(403).send("You are not admin");
+    }
+  } catch (err) {
+    if (err.message == "jwt expired") {
       return res.status(401).send("Token expired");
     }
     return res.status(401).send("Invalid Token");
